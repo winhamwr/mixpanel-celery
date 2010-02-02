0745f724f299919dbe95a48349580750bbee5888
winhamwr <winhamwr@gmail.com>
2010-02-02 15:10:04 -0500

    Back to 100% coverage

--- mixpanel/test/test_tasks.py
+++ mixpanel/test/test_tasks.py
@@ -2,11 +2,16 @@ import unittest
 import base64
 import simplejson
 import urllib
+import logging
 
 from mixpanel.tasks import EventTracker, FunnelEventTracker
 from mixpanel.conf import settings as mp_settings
 
 class EventTrackerTest(unittest.TestCase):
+    def setUp(self):
+        mp_settings.MIXPANEL_API_TOKEN = 'testtesttest'
+        mp_settings.MIXPANEL_API_SERVER = 'api.mixpanel.com'
+        mp_settings.MIXPANEL_TRACKING_ENDPOINT = '/track/'
 
     def test_handle_properties_w_token(self):
         et = EventTracker()
@@ -41,12 +46,41 @@ class EventTrackerTest(unittest.TestCase):
 
         self.assertEqual(expected_params, url_params)
 
+    def test_failed_request(self):
+        mp_settings.MIXPANEL_TRACKING_ENDPOINT = 'brokenurl'
+
+        et = EventTracker()
+        self.assertRaises(EventTracker.FailedEventRequest,
+                          et.run,
+                          'event_foo', {'test': 1})
+
     def test_run(self):
         # "correct" result obtained from: http://mixpanel.com/api/docs/console
         mp_settings.MIXPANEL_API_TOKEN = 'testtesttest'
 
         et = EventTracker()
         result = et.run('event_foo', {'test': 1})
+
+        self.assertTrue(result)
+
+    def test_old_run(self):
+        """non-recorded events should return False"""
+        mp_settings.MIXPANEL_API_TOKEN = 'testtesttest'
+
+        et = EventTracker()
+        # Times older than 3 hours don't get recorded according to: http://mixpanel.com/api/docs/specification
+        # equests will be rejected that are 3 hours older than present time
+        result = et.run('event_foo', {'time': 1245613885})
+
+        self.assertFalse(result)
+
+    def test_debug_logger(self):
+        et = EventTracker()
+        # Times older than 3 hours don't get recorded according to: http://mixpanel.com/api/docs/specification
+        # equests will be rejected that are 3 hours older than present time
+        result = et.run('event_foo', {'test': 1}, loglevel=logging.DEBUG)
+
+        self.assertTrue(result)
 
 class FunnelEventTrackerTest(unittest.TestCase):
     def test_afp_validation(self):
@@ -96,3 +130,5 @@ class FunnelEventTrackerTest(unittest.TestCase):
 
         fet = FunnelEventTracker()
         result = fet.run('funnel_foo', '1', '3', {'ip': '127.0.0.1', 'test': 1})
+
+        self.assertTrue(result)
