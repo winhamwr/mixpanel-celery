6bfa6c48a0dc7f92a51f7700201224c02acfbc72
winhamwr <winhamwr@gmail.com>
2010-08-12 11:22:30 -0400

    Fixed braindead install_required typo

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     packages=find_packages(),
     scripts=[],
     zip_safe=False,
-    install_requires=['celery=1.0.6'],
+    install_requires=['celery==1.0.6'],
     cmdclass = {},
     classifiers=[
         "Development Status :: 4 - Beta",
