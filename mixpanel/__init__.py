ae743bfd9d0767f7719c3eb8d7491dda5acd5022
winhamwr <winhamwr@gmail.com>
2013-05-22 18:46:53 -0400

    Bump the version to 0.6.0

--- mixpanel/__init__.py
+++ mixpanel/__init__.py
@@ -1,10 +1,10 @@
 """Asynchronous event tracking for Mixpanel"""
 
-VERSION = (0, 5, 0, '')
+VERSION = (0, 6, 0, '')
 
 __version__ = ".".join(map(str, VERSION[:-1]))
 __release__ = ".".join(map(str, VERSION))
 __author__ = "Wes Winham"
-__contact__ = "wes@policystat.com"
-__homepage__ = "http://github.com/winhamwr/mixpanel-celery/"
+__contact__ = "winhamwr@gmail.com"
+__homepage__ = "http://winhamwr.github.io/mixpanel-celery/"
 __docformat__ = "restructuredtext"
