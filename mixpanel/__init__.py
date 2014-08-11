8d19496356bc1cda1db2329690da52743aa93910
winhamwr <winhamwr@gmail.com>
2014-08-11 15:01:12 -0400

    Bumped version to v0.8.1dev

--- mixpanel/__init__.py
+++ mixpanel/__init__.py
@@ -1,6 +1,6 @@
 """Asynchronous event tracking for Mixpanel"""
 
-VERSION = (0, 8, 0, '')
+VERSION = (0, 8, 1, 'dev')
 
 __version__ = ".".join(map(str, VERSION[:-1]))
 __release__ = ".".join(map(str, VERSION))
